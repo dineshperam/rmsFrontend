4ec565c8787c0e2f676c5c25318e02bbe41c0dab
Dinesh Peram <dinesh.peram@brillio.com>
2025-02-06 18:57:43 +0530

    first commit

---
+++ react-admin-dashboard-master/src/pages/AnalyticsPage.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/common/Header";
+
+import OverviewCards from "../components/analytics/OverviewCards";
+import RevenueChart from "../components/analytics/RevenueChart";
+
+const AnalyticsPage = () => {
+	return (
+		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
+			<Header title={"Analytics Dashboard"} />
+
+			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+				<OverviewCards />
+				<RevenueChart />
+			</main>
+		</div>
+	);
+};
+export default AnalyticsPage;
